72c8df0501582aa34b452b479767a2793e48ad23
zack.zhang <zackzhang@augmentum.com.cn>
2021-02-05 08:45:20 +0800

    leetcode/golang: add two sum go implement

--- code/leetcode/golang/easy/leetcode/array/two_sum.go
+++ code/leetcode/golang/easy/leetcode/array/two_sum.go
@@ -1,7 +1,15 @@
 package array
 
-import "fmt"
+func TwoSum(nums []int, target int) []int {
+	// build map to store: key is num element, and value is index
+	m := make(map[int]int, 10)
+	for i, v := range nums {
+		if repairIndex, ok := m[target-v]; ok {
+			return []int{repairIndex, i}
+		} else {
+			m[v] = i
+		}
+	}
 
-func TwoSum() {
-    fmt.Println("hello twoSum *** ")
+	return nil
 }
