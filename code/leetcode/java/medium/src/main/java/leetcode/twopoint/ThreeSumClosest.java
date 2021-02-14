3208df8c417cf2b5ee46a60a0c75b0dd68f61aa9
Alice52 <zzhang_xz@163.com>
2021-02-14 21:06:38 +0800

    leetcode/java: add implement of 3-sum-closest issue

---
+++ code/leetcode/java/medium/src/main/java/leetcode/twopoint/ThreeSumClosest.java
@@ -0,0 +1,9 @@
+package leetcode.twopoint;
+
+/**
+ * @see leetcode.array.ThreeSumClosest
+ * @author zack <br>
+ * @create 2021-02-14 20:24 <br>
+ * @project leetcode <br>
+ */
+public class ThreeSumClosest {}
