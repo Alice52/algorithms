9ad9180c9d1bce68ebbef64fccd47d311ae4c32a
Alice52 <zzhang_xz@163.com>
2021-02-15 21:17:42 +0800

    interview/golang: add implement of list flip

--- code/leetcode/golang/common/api/model/ListNode.go
+++ code/leetcode/golang/common/api/model/ListNode.go
@@ -1,7 +1,17 @@
 package model
 
+import "fmt"
+
 // singly-linked
 type ListNode struct {
     Val  int
     Next *ListNode
+}
+
+func PrintListNode(node *ListNode) {
+    for node.Next != nil {
+        fmt.Println(node.Val)
+        node = node.Next
+    }
+    fmt.Println(node.Val)
 }
