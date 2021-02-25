4f265fa5cee18caac5d0ae006e9f3cd1adc2ea3b
zack.zhang <zackzhang@augmentum.com.cn>
2021-02-25 11:01:06 +0800

    leetcode/golang: add implement of leetcode about array contains specified duplicate count element

--- code/leetcode/golang/easy/leetcode/test/array_test.go
+++ code/leetcode/golang/easy/leetcode/test/array_test.go
@@ -60,3 +60,9 @@ func TestIntervalIntersection(t *testing.T) {
         [][]int{{1, 5}, {8, 12}, {15, 24}, {25, 26}})
     fmt.Printf("intersection ii is: %v\n", result)
 }
+
+func TestDuplicateJudgeLessSpecifiedCount(t *testing.T) {
+
+    result := array.DuplicateJudgeLessSpecifiedCount([]int{1, 1, 1, 3}, 3)
+    fmt.Printf("duplicate judge less specified count: %v\n", result)
+}
