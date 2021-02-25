0c09402c22d7b30d44348552e59a18510499e19e
zack.zhang <zackzhang@augmentum.com.cn>
2021-02-25 10:13:43 +0800

    leetcode/golang: re-struct module to remove easy, mdium, hard modules

--- code/leetcode/golang/easy/leetcode/test/array_test.go
+++ code/leetcode/golang/easy/leetcode/test/array_test.go
@@ -60,3 +60,25 @@ func TestIntervalIntersection(t *testing.T) {
         [][]int{{1, 5}, {8, 12}, {15, 24}, {25, 26}})
     fmt.Printf("intersection ii is: %v\n", result)
 }
+
+func Test(t *testing.T) {
+
+    nums := []int{0, 0, 0, 0}
+    result := array.ThreeSum(nums)
+    fmt.Printf("result: %v", result)
+}
+
+func TestFourSum(t *testing.T) {
+
+    result := array.FourSumV3([]int{0, 1, 5, 0, 1, 5, 5, -4}, 11)
+
+    fmt.Printf("%v", result)
+}
+
+func TestFourSumII(t *testing.T) {
+
+    println(array.FourSumII([]int{1, 2},
+        []int{-2, -1},
+        []int{-1, 2},
+        []int{0, 2}))
+}
--- code/leetcode/golang/easy/leetcode/test/list_test.go
+++ code/leetcode/golang/easy/leetcode/test/list_test.go
@@ -58,3 +58,103 @@ func TestGetIntersectionNode(t *testing.T) {
         fmt.Printf("intersection node value: %v\n", result.Val)
     }
 }
+
+func TestAddTwoSum(t *testing.T) {
+
+    a := &model.ListNode{
+        Val: 2,
+        Next: &model.ListNode{
+            Val: 4,
+            Next: &model.ListNode{
+                Val:  3,
+                Next: nil,
+            },
+        },
+    }
+
+    b := &model.ListNode{
+        Val: 5,
+        Next: &model.ListNode{
+            Val: 6,
+            Next: &model.ListNode{
+                Val:  4,
+                Next: nil,
+            },
+        },
+    }
+
+    result := list.AddTwoSum(a, b)
+    for result != nil {
+        fmt.Print(result.Val)
+        result = result.Next
+    }
+
+    fmt.Println()
+
+    a2 := &model.ListNode{
+        Val: 9,
+        Next: &model.ListNode{
+            Val: 9,
+            Next: &model.ListNode{
+                Val:  9,
+                Next: nil,
+            },
+        },
+    }
+
+    b2 := &model.ListNode{
+        Val:  1,
+        Next: nil,
+    }
+
+    result2 := list.AddTwoSum(a2, b2)
+    for result2 != nil {
+        fmt.Print(result2.Val)
+        result2 = result2.Next
+    }
+    fmt.Println()
+}
+
+func TestMaxArea(t *testing.T) {
+    nums := []int{1, 2, 3}
+    result := list.MaxArea(nums)
+    fmt.Println(result)
+}
+
+func TestRemoveDuplicateOfSortedList(t *testing.T) {
+    node := &model.ListNode{
+        Val: 1,
+        Next: &model.ListNode{
+            Val: 2,
+            Next: &model.ListNode{
+                Val: 3,
+                Next: &model.ListNode{
+                    Val:  3,
+                    Next: nil,
+                },
+            },
+        },
+    }
+
+    result := list.RemoveDuplicateOfSortedList(node)
+    model.PrintListNode(result)
+}
+
+func TestRemoveDuplicateOfSortedListII(t *testing.T) {
+    node := &model.ListNode{
+        Val: 1,
+        Next: &model.ListNode{
+            Val: 2,
+            Next: &model.ListNode{
+                Val: 3,
+                Next: &model.ListNode{
+                    Val:  3,
+                    Next: nil,
+                },
+            },
+        },
+    }
+
+    result := list.RemoveDuplicateOfSortedListII(node)
+    model.PrintListNode(result)
+}
