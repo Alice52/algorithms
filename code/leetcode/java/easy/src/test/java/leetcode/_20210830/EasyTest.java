5716b70d6a6d3c4d72addce36fcc0f6e4e712e0a
Alice52 <zzhang_xz@163.com>
2021-09-13 20:01:58 +0800

    leetcode/java: daily leetcode

--- code/leetcode/java/easy/src/test/java/leetcode/_20210830/EasyTest.java
+++ code/leetcode/java/easy/src/test/java/leetcode/_20210830/EasyTest.java
@@ -1,9 +1,11 @@
 package leetcode._20210830;
 
+import cn.hutool.core.util.StrUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * @author zack <br>
@@ -12,6 +14,206 @@ import java.util.List;
  */
 @Slf4j
 public class EasyTest {
+
+    public boolean checkValidString(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        int commCount=0;
+        for  (int i=0; i<s.length(); i++) {
+            char cur = s.charAt(i);
+            if (cur =='*') {
+                stack.add(cur);
+                commCount++;
+            } else if (cur == '(') {
+                stack.add(cur);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                int leftIndex = stack.lastIndexOf('(');
+                if (leftIndex >= 0) {
+                    stack.remove(leftIndex);
+                } else {
+                    stack.pop();
+                    commCount--;
+                }
+            }
+        }
+
+        //  ( * * * (
+        commCount=0;
+        while (!stack.isEmpty()) {
+            char pop = stack.pop();
+            if (pop == '*') {
+                commCount++;
+            } else {
+                if (commCount <=0) {
+                    return false;
+                }
+                commCount--;
+            }
+        }
+
+        return true;
+    }
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        dp[0]=0;
+        for (int i=0; i< coins.length; i++) {
+            for (int j=0; j<=amount; j++) {
+                if (j >= coins[i]) {
+                    dp[j] = Math.min(dp[j], dp[j-coins[i]]+1);
+                }
+            }
+        }
+
+        for (int a : dp) {
+            System.out.println("dp: " + a);
+        }
+
+        return 1;
+    }
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        backtracking(0, target, 0, candidates, new HashSet<Integer>());
+
+        return res;
+    }
+
+    void backtracking(int start, int target, int preSum, int[] candidates, Set<Integer> path) {
+        if (preSum == target) {
+            res.add(new ArrayList(path));
+            return;
+        }
+
+        for (int i=start; i<candidates.length; i++) {
+            if (preSum > target) {
+                break;
+            }
+
+            path.add(candidates[i]);
+            preSum+=candidates[i];
+            backtracking(i, target, preSum, candidates, path);
+            preSum-=candidates[i];
+            path.remove(candidates[i]);
+        }
+    }
+
+    @Test
+    public void test77() {
+        combinationSum(new int[]{2,3,6,7}
+        , 7);
+    }
+
+
+    public List<List<Integer>> combine(int n, int k) {
+        backtrack(1, n, k, new HashSet<Integer>());
+
+        return res;
+    }
+
+
+    void backtrack(int start, int n, int k, Set<Integer> track) {
+        if (track.size() == k) {
+            res.add(new ArrayList(track));
+            return;
+        }
+
+        for (int i=start; i<= n; i++) {
+
+            track.add(i);
+            backtrack(i+1, n, k, track);
+            track.remove(i);
+        }
+    }
+
+    @Test
+    public void testReg() {
+            }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+
+        // Set<String> visited = new HashSet<>();
+        // boolean[] visited = new boolean[wordList.size()];
+        Queue<String> queue = new LinkedList<>();
+        int step =1;
+
+        queue.offer(beginWord);
+        // visited.add(beginWord);
+        wordList.remove(beginWord);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size >0) {
+                size--;
+                String tmp = queue.poll();
+                if (tmp.equals(endWord)) {
+                    return step;
+                }
+
+                Iterator<String> iterator = wordList.iterator();
+                while (iterator.hasNext()) {
+                    String next = iterator.next();
+                    if (!canConvert(tmp, next)) {
+                        continue;
+                    }
+                    queue.offer(next);
+                    iterator.remove();
+                }
+            }
+
+            step++;
+        }
+
+        return 0;
+    }
+
+    public boolean canConvert(String s1, String s2) {
+        int count = 0;
+        for (int i = 0; i < s1.length(); ++i) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                ++count;
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+        return count == 1;
+    }
+
+    @Test
+    public void test739() {
+
+        dailyTemperatures(new int[] {89, 62, 70, 58, 47, 47, 46, 76, 100, 70});
+    }
+
+    public int[] dailyTemperatures(int[] t) {
+        int len = t.length;
+        int[] dp = new int[len];
+
+        Arrays.fill(dp, 0);
+        // 从右向左遍历
+        for (int i = len - 2; i >= 0; i--) {
+            int j = i + 1;
+            while (j < len) {
+                if (t[j] > t[i]) {
+                    dp[i] = j - i;
+                    break;
+                } else if (dp[j] == 0) {
+                    dp[i] = 0;
+                    break;
+                }
+
+                j += dp[j];
+            }
+        }
+
+        return dp;
+    }
 
     @Test
     public void test1011() {
